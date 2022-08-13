83147ac2a6b8815fd71ce74711f2afe979ef508c
JaswalAshish48 <jaswalashish48@gmail.com>
2022-08-13 18:10:58 +0530

    veux: Its Snow Time
    Signed-off-by: JaswalAshish48 <jaswalashish48@gmail.com>

--- evolution_veux.mk
+++ evolution_veux.mk
@@ -8,13 +8,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from veux device
 $(call inherit-product, device/xiaomi/veux/device.mk)
 
-PRODUCT_NAME := lineage_veux
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+TARGET_USES_AOSP_RECOVERY := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := evolution_veux
 PRODUCT_DEVICE := veux
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -23,6 +28,6 @@ PRODUCT_MODEL := POCO X4 Pro 5G
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="veux-user 11 RKQ1.211001.001 V13.0.10.0.RKCMIXM release-keys"
+    PRIVATE_BUILD_DESC="veux-user 11 RKQ1.211001.001 V13.0.14.0.RKCMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/veux_global/veux:11/RKQ1.211001.001/V13.0.10.0.RKCMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/veux_p_global/veux:11/RKQ1.211001.001/V13.0.14.0.RKCMIXM:user/release-keys
