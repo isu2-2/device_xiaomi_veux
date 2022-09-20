NandagopalPV/lineage_device_xiaomi_veux

veux: Use libhidlbase from vndk v32 for vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so
["_ZN7android8hardware7details17gBnConstructorMapE" and "_ZN7android8hardware7details17gBsConstructorMapE" have been removed on Android 13]

[@Joker-V2: Adapt to our blob]

Signed-off-by: Lucchetto <lucchetto.tie@live.com>
Signed-off-by: Joker-V2 <nipinna0@gmail.com>
Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
Change-Id: Ia052716c998d296e5f966abb2d0944a69ffa5f2e

extract-files.sh
@@ -14,7 +14,11 @@ function blob_fixup() {
         vendor/lib64/camera/components/com.qti.node.mialgocontrol.so)
             llvm-strip --strip-debug  "${2}"
             "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
-            ;;    
+            ;;
+        vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
+            "${PATCHELF_0_8}" --remove-needed "libhidlbase.so" "${2}"
+            sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
+            ;;        
     esac
 }
 

isu2-2/device_xiaomi_veux

Signed-off-by: JaswalAshish48 <jaswalashish48@gmail.com>

extract-files.sh
@@ -55,6 +55,14 @@ function blob_fixup() {
             llvm-strip --strip-debug  "${2}"
             "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;    
+        vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so)
+            "${PATCHELF}" --remove-needed "libhidlbase.so" "${2}"
+            sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
+            ;;
+        vendor/lib64/com.fingerprints.extension@1.0.so)
+            "${PATCHELF}" --remove-needed "libhidlbase.so" "${2}"
+            sed -i "s/libhidltransport.so/libhidlbase-v32.so\x00/" "${2}"
+            ;;        
     esac
 }
 
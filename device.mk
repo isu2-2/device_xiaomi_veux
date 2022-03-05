362c4e655abc2a3fc23e094797becf76f581b6e6
Lucchetto <lucchetto.tie@live.com>
2022-03-04 22:48:16 -0300

    veux: Use libhidlbase from vndk v32 for vendor/lib64/libvendor.goodix.hardware.biometrics.fingerprint@2.1.so
    ["_ZN7android8hardware7details17gBnConstructorMapE" and "_ZN7android8hardware7details17gBsConstructorMapE" have been removed on Android 13]
    
    [@Joker-V2: Adapt to our blob]
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>
    Signed-off-by: JaswalAshish48 <jaswalashish48@gmail.com>

--- device.mk
+++ device.mk
@@ -192,6 +192,10 @@ PRODUCT_PACKAGES += \
     qcom.fmradio \
     qcom.fmradio.xml
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
