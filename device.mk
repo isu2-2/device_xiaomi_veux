de52ea3acd9d5241713136d7734350d6f9fcbbe7
anant-goel <100245626+anant-goel@users.noreply.github.com>
2026-06-08 21:07:51 +0530

    veux: added missing audio file

--- device.mk
+++ device.mk
@@ -99,7 +99,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/sku1_AW88261.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku1_AW88261.xml \
     $(LOCAL_PATH)/configs/audio/sku1_FS1962.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku1_FS1962.xml \
     $(LOCAL_PATH)/configs/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sound_trigger_mixer_paths.xml \
-    $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sound_trigger_platform_info.xml
+    $(LOCAL_PATH)/configs/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sound_trigger_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio/sku_holi/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_holi/audio_io_policy.conf
 
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/holi/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio/$(AUDIO_SKU_DIR)/audio_io_policy.conf \
